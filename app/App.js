f786c708919beb074620be98d00f584d79fb9fb8
Frank Leal <franklealorg@gmail.com>
2019-08-22 21:03:50 -0500

    moving theme to globals

--- app/App.js
+++ app/App.js
@@ -1,48 +1,13 @@
 import React, { Component } from 'react';
 import { ThemeProvider } from 'react-native-elements';
 import AppStackNavigator from './config/RouterNew';
-
-const theme = {
-  Input: {
-    containerStyle: {
-      backgroundColor: 'white',
-      borderBottomWidth: 0,
-      marginVertical: 7,
-    },
-    inputContainerStyle: {
-      borderBottomColor: 'rgba(255, 255, 255, 0)',
-      height: 50,
-    },
-    inputStyle: {
-      fontSize: 18,
-      color: 'black',
-      borderRadius: 0
-    }
-  },
-  Text: {
-    fontSize: 30,
-  },
-  Button: {
-    containerStyle: {
-      marginVertical: 7,
-    },
-    buttonStyle: {
-      height: 50,
-      borderRadius: 0,
-      backgroundColor: '#BD742C',
-    }
-  },
-  Icon: {
-    color: 'white'
-  }
-};
-
+import Globals from './config/Globals';
 
 class App extends Component {
   render() {
     console.disableYellowBox = true;
     return (
-      <ThemeProvider theme={theme}>
+      <ThemeProvider theme={Globals.THEME}>
         <AppStackNavigator />
       </ThemeProvider>
     );
