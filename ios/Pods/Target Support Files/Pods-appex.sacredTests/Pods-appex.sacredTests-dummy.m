f939ae2cca5cea3bfdd5b6e37f0afbe917fc6ec0
Frank Leal <falg2@hotmail.com>
2019-10-09 15:22:23 -0500

    ios settings

---
+++ ios/Pods/Target Support Files/Pods-appex.sacredTests/Pods-appex.sacredTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_appex_sacredTests : NSObject
+@end
+@implementation PodsDummy_Pods_appex_sacredTests
+@end
--- ios/appex.sacredTests/appex.sacredTests.m
+++ ios/appex.sacredTests/appex.sacredTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface appex.sacredTests : XCTestCase
+@interface sacredTests : XCTestCase
 
 @end
 
-@implementation appex.sacredTests
+@implementation sacredTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
